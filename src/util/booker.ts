73971c33cc4b37143e9ff0d654a7861d838fd702
Manuel Stella <35688032+Styddy@users.noreply.github.com>
2024-02-08 19:56:50 +0100

    Changed behaviour of the autobooker (#4)

--- src/util/booker.ts
+++ src/util/booker.ts
@@ -35,78 +35,107 @@ async function autobookUsers(users: IUser[], i18n: TelegrafI18n) {
 async function autobookUser(user: IUser, i18n: TelegrafI18n) {
   const login = await manageLogin(user, i18n);
   let message = login.message;
-  const getFormattedDate = (date: Date) => format(date, "yyyy-MM-dd");
   if (!login.error) {
-    const parkingsResponse = await getOggetti(
-      login.loginResponse.sessionId,
-      32
-    );
+    const sessionId = login.loginResponse.sessionId;
+    const parkingsResponse = await getOggetti(sessionId, 32);
     if (typeof parkingsResponse.error !== "boolean") {
       message = parkingsResponse.error.message;
     } else if (
       !parkingsResponse.error &&
       typeof parkingsResponse.response !== "boolean"
     ) {
-      const posti = parkingsResponse.response as {
-        nome: string;
-        pk_oggetti: string;
-      }[];
-      const posto = posti.find((posto) => posto.nome === user.parking);
-      const now = new Date();
-      let millisTillMidnight: number =
-        new Date(
-          now.getFullYear(),
-          now.getMonth(),
-          now.getDate(),
-          23,
-          59,
-          59,
-          999
-        ).getTime() - now.getTime();
+      const posto = getPosto(parkingsResponse, user.parking);
+      const secondsTillMidnight: number = getSecondsTillMidnight();
 
-      if (millisTillMidnight < 0) {
-        millisTillMidnight = 0;
-      }
+      await sleep(secondsTillMidnight);
+      message = await manageBooking(
+        user,
+        i18n,
+        message,
+        sessionId,
+        posto.pk_oggetti
+      );
+    }
+  }
+  sendMessage(user._id, message);
+  await sleep(0.5);
+}
 
-      logger.debug(undefined, "MillisTillMidnight " + millisTillMidnight);
+async function manageBooking(
+  user: IUser,
+  i18n: TelegrafI18n,
+  message: string,
+  sessionId: string,
+  idParking: string
+) {
+  const getFormattedDate = (date: Date) => format(date, "yyyy-MM-dd");
+  let parkingBooked = false;
+  let requestTimeout = false;
+  let request = 0;
 
-      await new Promise((resolve) => setTimeout(resolve, millisTillMidnight));
+  while (!parkingBooked && !requestTimeout) {
+    const currentDate = new Date();
+    const nextDateMorning = currentDate;
+    nextDateMorning.setDate(currentDate.getDate() + 31);
 
-      let parkingBooked = false;
-      let request = 0;
+    if (request > 300) {
+      requestTimeout = true;
+    }
 
-      while (!parkingBooked && request < 300) {
-        logger.debug(undefined, "Request " + request);
-        const currentDate = new Date();
-        const nextDateMorning = currentDate;
-        nextDateMorning.setDate(currentDate.getDate() + 31);
-        request++;
-        const prenotazioneResponse = await setPrenotazione(
-          login.loginResponse.sessionId,
-          posto.pk_oggetti,
-          getFormattedDate(nextDateMorning)
-        );
-
+    if (!parkingBooked) {
+      request++;
+      setPrenotazione(
+        sessionId,
+        idParking,
+        getFormattedDate(nextDateMorning)
+      ).then((prenotazioneResponse) => {
         if (typeof prenotazioneResponse.error !== "boolean") {
-          await new Promise((resolve) => setTimeout(resolve, 30));
           message = prenotazioneResponse.error.message;
         } else if (
           !prenotazioneResponse.error &&
           typeof prenotazioneResponse.response !== "boolean"
         ) {
-          logger.debug(undefined, "A buon fine " + request);
           parkingBooked = true;
           message = `${prenotazioneResponse.response as string}\n${i18n.t(
             user.language,
             "scenes.bookings.booking_confirmed",
-            { date: getFormattedDate(nextDateMorning), object: user.parking }
+            {
+              date: getFormattedDate(nextDateMorning),
+              object: user.parking,
+            }
           )}`;
         }
-      }
+      });
+      await sleep(0.01);
     }
   }
-  sendMessage(user._id, message);
-  await sleep(0.5);
+  return message;
+}
+
+function getPosto(parkingResponse: IResponse, userParking: string) {
+  const posti = parkingResponse.response as {
+    nome: string;
+    pk_oggetti: string;
+  }[];
+  return posti.find((posto) => posto.nome === userParking);
+}
+
+function getSecondsTillMidnight(): number {
+  const now = new Date();
+  let millisTillMidnight =
+    new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      23,
+      59,
+      59,
+      999
+    ).getTime() - now.getTime();
+  if (millisTillMidnight < 0) {
+    millisTillMidnight = 0;
+  }
+  return (millisTillMidnight + 1) / 1000;
 }
 
 export async function sendMessage(
